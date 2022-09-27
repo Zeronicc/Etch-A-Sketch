f4ea98c2f33874125cd50b71d7ecfa2e7bdefc75
Zeronicc <zeronic4465@gmail.com>
2022-09-27 17:37:48 -0500

    All basic elements implemented

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const GRID_SIZE = 256;
+
 let container = document.querySelector(".container");
+container.style.display = 'none';
+
 const page = document.querySelector(".page");
 
+let gridSquares = document.getElementsByClassName("squares");
 let MODE = 'color';
 let title = document.querySelector("#title");
 let colorBtn = document.querySelector("#colorMode");
 let eraserBtn = document.querySelector("#erase");
 let clearBtn = document.querySelector("#clearBtn");
+let blackBtn = document.querySelector('#blackBtn');
 let addContainer = document.createDocumentFragment();
 let colorInput = document.querySelector("#color");
 let currentColor = '#333333';
@@ -18,10 +23,23 @@ function setMode(newMode){
     MODE = newMode;
 }
 function clearGrid(){
-    container.innerHTML = ''
+    for(const gridSquare of gridSquares){
+        gridSquare.style.backgroundColor = '#fefefe';
+    }
 }
+function setCurrentGrid(newGrid){
 
+}
+function blackTakeOver(){
+    for(const gridSquare of gridSquares){
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)
+        ;
+    }
+}
 colorInput.oninput = (e) => setCurrentColor(e.target.value);
+
 
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true);
@@ -32,10 +50,11 @@ startBtn.id = "start";
 startBtn.textContent = "Start Sketching";
 title.appendChild(startBtn);
 
-startBtn.addEventListener('click', (e) => {showGrid(); title.removeChild(startBtn)})
+startBtn.addEventListener('click', (e) => {showGrid(); title.removeChild(startBtn); container.style.display = 'flex'})
 eraserBtn.addEventListener('click', () => setMode('eraser'))
 colorBtn.addEventListener('click', () => setMode('color'))
 clearBtn.addEventListener('click', () => clearGrid())
+blackBtn.addEventListener('click', () => blackTakeOver())
 
 function showGrid(){    
 for(let i = 0; i < GRID_SIZE; i++){
