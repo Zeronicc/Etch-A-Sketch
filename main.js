3b2f939d3b755d84eef31fa86366837713cc62d5
Zeronicc <zeronic4465@gmail.com>
2022-09-28 03:39:04 -0500

    Used DOM to display optional grid size

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-const GRID_SIZE = 256;
-
 let container = document.querySelector(".container");
 container.style.display = 'none';
-
 const page = document.querySelector(".page");
 
 let gridSquares = document.getElementsByClassName("squares");
@@ -27,40 +24,69 @@ function clearGrid(){
         gridSquare.style.backgroundColor = '#fefefe';
     }
 }
-function setCurrentGrid(newGrid){
-
-}
 function blackTakeOver(){
     for(const gridSquare of gridSquares){
         const randomR = Math.floor(Math.random() * 256);
         const randomG = Math.floor(Math.random() * 256);
         const randomB = Math.floor(Math.random() * 256);
-        ;
-    }
+        gridSquare.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`; 
+    }  
 }
+
 colorInput.oninput = (e) => setCurrentColor(e.target.value);
 
-
-let mouseDown = false
+let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
 let startBtn = document.createElement('button');
 startBtn.id = "start";
-startBtn.textContent = "Start Sketching";
+startBtn.textContent = "Get Started";
 title.appendChild(startBtn);
 
-startBtn.addEventListener('click', (e) => {showGrid(); title.removeChild(startBtn); container.style.display = 'flex'})
+let gridSelector = document.createElement('div');
+function showOptions(){
+    let btnContainer = document.createElement("div");
+    let btn10 = document.createElement("button");
+    let btn16 = document.createElement("button");
+    let btn20 = document.createElement("button");
+    let btn30 = document.createElement("button");
+
+    btn10.textContent = "10 X 10";
+    btn16.textContent = "16 X 16";
+    btn20.textContent = "20 X 20";
+    btn30.textContent = "30 X 30";
+
+    btnContainer.className ="gridSizes"
+    
+    title.appendChild(btnContainer)
+    btnContainer.appendChild(btn10);
+    btnContainer.appendChild(btn16);
+    btnContainer.appendChild(btn20);
+    btnContainer.appendChild(btn30);
+    
+    btn10.addEventListener('click', () => showGrid(10));
+    btn16.addEventListener('click', () => showGrid(16));
+    btn20.addEventListener('click', () => showGrid(20));
+    btn30.addEventListener('click', () => showGrid(30));
+}
+
+startBtn.addEventListener('click', (e) => { showOptions();title.removeChild(startBtn);})
 eraserBtn.addEventListener('click', () => setMode('eraser'))
 colorBtn.addEventListener('click', () => setMode('color'))
 clearBtn.addEventListener('click', () => clearGrid())
-blackBtn.addEventListener('click', () => blackTakeOver())
+blackBtn.addEventListener('click', () => {blackTakeOver(); setMode('blackMode')})
 
-function showGrid(){    
-for(let i = 0; i < GRID_SIZE; i++){
+function showGrid(gridSize){    
+    container.style.display = 'inline-grid';
+    container.innerHTML = '';
+    let grid = gridSize * gridSize;
+    for(let i = 0; i < grid; i++){
         let div = document.createElement('div');
         div.className = "squares";
         addContainer.appendChild(div);
+        container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
         div.addEventListener('mouseover', changeColor);
         div.addEventListener('mousedown', changeColor);
     }
@@ -72,5 +98,7 @@ function changeColor(e){
     e.target.style.backgroundColor = currentColor
     }else if(MODE == 'eraser'){
     e.target.style.backgroundColor = '#fefefe'
+    }else if(MODE == 'blackMode'){
+    e.target.style.backgroundColor = 'black'
     }
 }
